6e62fb73086e678fb007fa7010b35e924ae03113
Victor Ochoa <victor.ochoa@gtim.mx>
2024-09-25 19:13:20 -0600

    se agregaron variables de entorno para apis

--- src/app/flights/services/flights.service.ts
+++ src/app/flights/services/flights.service.ts
@@ -2,18 +2,15 @@ import { Injectable } from '@angular/core';
 import { ApiResponse, Flight, NWeather, Weather } from '../interface/flight';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FlightsService {
-  private apiUrl = 'http://localhost:3000/api';
-  constructor(private http: HttpClient) { }
-  
-  // getWeatherByCoordinates(latitude: number , longitude: number): Observable<NWeather> {
-  //   const url = `${this.apiUrl}/weather/getByCoordinates?latitude=${latitude}&longitude=${longitude}`;
-  //   return this.http.get<NWeather>(url);
-  // }
+  private apiUrl: string = environment.apiUrl;
+  constructor(private http: HttpClient) {
+  }
 
   getWeatherReport(page:number, pageSize:number) : Observable<ApiResponse<Flight>> {
     const url = `${this.apiUrl}/flights/getWeatherReport?page=${page}&pageSize=${pageSize}`;
